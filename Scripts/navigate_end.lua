a3e8f6e0437a9ee5cd26582dd4b4e98c3f6f4b0e
samlletas <samuel.macedo.v@outlook.com>
2024-05-20 15:05:02 -0700

    feat: add commands to navigate home/end

---
+++ Scripts/navigate_end.lua
@@ -0,0 +1,27 @@
+-- @author samlletas
+-- @description Go to end of loop/item/project
+
+local loopStart, loopEnd = reaper.GetSet_LoopTimeRange(false, false, 0, 0, false)
+local midiEditor = reaper.MIDIEditor_GetActive()
+
+local function runOnMidiEditor(midiEditor, isLoopSelected)
+    if isLoopSelected then
+        reaper.MIDIEditor_OnCommand(midiEditor, 40439) -- [MIDI Editor] Move cursor to end of loop
+    else
+        reaper.MIDIEditor_OnCommand(midiEditor, 40037) -- [MIDI Editor] View: Go to end of file
+    end
+end
+
+local function runOnMain(isLoopSelected)
+    if isLoopSelected then
+        reaper.Main_OnCommand(40633, 0) -- [Main] Go to end of loop
+    else
+        reaper.Main_OnCommand(40043, 0) -- [Main] Transport: Go to end of project
+    end
+end
+
+if midiEditor then
+    runOnMidiEditor(midiEditor, loopStart ~= loopEnd)
+else
+    runOnMain(loopStart ~= loopEnd)
+end
---
+++ Scripts/navigate_home.lua
@@ -0,0 +1,27 @@
+-- @author samlletas
+-- @description Go to start of loop/item/project
+
+local loopStart, loopEnd = reaper.GetSet_LoopTimeRange(false, false, 0, 0, false)
+local midiEditor = reaper.MIDIEditor_GetActive()
+
+local function runOnMidiEditor(midiEditor, isLoopSelected)
+    if isLoopSelected then
+        reaper.MIDIEditor_OnCommand(midiEditor, 40438) -- [MIDI Editor] Move cursor to start of loop
+    else
+        reaper.MIDIEditor_OnCommand(midiEditor, 40036) -- [MIDI Editor] View: Go to start of file
+    end
+end
+
+local function runOnMain(isLoopSelected)
+    if isLoopSelected then
+        reaper.Main_OnCommand(40632, 0) -- [Main] Go to start of loop
+    else
+        reaper.Main_OnCommand(40042, 0) -- [Main] Transport: Go to start of project
+    end
+end
+
+if midiEditor then
+    runOnMidiEditor(midiEditor, loopStart ~= loopEnd)
+else
+    runOnMain(loopStart ~= loopEnd)
+end
