6792fdd666ba7832f056dbbcdfe1da6837dc0b32
samlletas <samuel.macedo.v@outlook.com>
2024-10-02 16:24:39 -0700

    feat: improve record_once script behavior

--- Scripts/record_once.lua
+++ Scripts/record_once.lua
@@ -10,13 +10,21 @@ local function stopRecordingAfterLoop()
   local position = math.max(reaper.GetPlayPosition(), 0)
   local loopStart, loopEnd = reaper.GetSet_LoopTimeRange(false, true, 0, 0, false)
 
-  if state & RECORDING == 0 or loopStart == loopEnd or position > loopEnd then
+  -- Exit when recording stops or no time selection exists
+  if state & RECORDING == 0 or loopStart == loopEnd then
     return
   end
 
+  -- When repeat is disabled, stop recording after loop
+  if position > loopEnd then
+    reaper.Main_OnCommand(1016, 0) -- [Main] Transport: Stop
+    return
+  end
+
+  -- When repeat is enabled, play recording after loop
   if state & PLAYING > 0 then
     if position < positionPrev then
-      reaper.Main_OnCommand(1007, 0) -- play
+      reaper.Main_OnCommand(1007, 0) -- [Main] Transport: Play
       return
     end
     positionPrev = position
@@ -25,5 +33,6 @@ local function stopRecordingAfterLoop()
   reaper.defer(stopRecordingAfterLoop)
 end
 
-reaper.Main_OnCommand(1013, 0) -- record
+reaper.Main_OnCommand(40630, 0) -- [Main] Go to start of time selection
+reaper.Main_OnCommand(1013, 0)  -- [Main] Transport: Record
 stopRecordingAfterLoop()
